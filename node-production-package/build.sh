65839d3d87fcaba1917d8b204c7c5f36016640ed
RayGervais <raygervais@hotmail.ca>
2018-12-08 21:50:54 -0500

    update(Dockerfile): Adds Node and Angular Docker workflows

---
+++ node-production-package/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e 
+# ex: build.sh  -u test -n node_test
+
+while getopts u:n: option
+do
+case "${option}"
+in
+u) USER=${OPTARG};;
+n) APP=${OPTARG};;
+esac
+done
+
+# Build Container
+docker build -t $USER/$APP .
+
+# List Containers
+docker images
---
+++ node-production-package/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e 
+# ex: run.sh -u test -n node_test -p 8080
+
+while getopts u:n:p: option
+do
+case "${option}"
+in
+u) USER=${OPTARG};;
+n) APP=${OPTARG};;
+p) PORT=${OPTARG};;
+esac
+done
+
+# Run Docker Container in Background
+docker run -p $PORT:8080 -d $USER/$APP
+
+# Show Running Instances
+docker ps
